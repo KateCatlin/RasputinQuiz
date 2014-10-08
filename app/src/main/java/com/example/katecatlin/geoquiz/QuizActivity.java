ed54ded17a0a739bf51a05e0575bd768de0e8602
Kate Catlin <kate.catlin@detroitlabs.com>
2014-10-07 20:36:28 -0400

    Adding log messages

--- app/src/main/java/com/example/katecatlin/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/katecatlin/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.katecatlin.geoquiz;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 
 
 public class QuizActivity extends Activity {
-
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -51,6 +52,7 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
 
@@ -109,6 +111,36 @@ public class QuizActivity extends Activity {
 
 
         updateQuestion();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
